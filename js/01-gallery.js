0aa3b1f7ab0f57c9f90bfb0ec8150037fcc03c18
TetyanaVynnyk <kharchenko1988@bigmir.net>
2022-11-11 23:10:11 +0200

    Task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const mainGallery = document.querySelector(".gallery");
+
+const makeGalleryItem = ({
+  preview,
+  original,
+  description,
+}) => `<div class="gallery__item"> <a class="gallery__link" href=${original}>
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a></div>`;
+const makeGalleryItems = galleryItems.map(makeGalleryItem).join("");
+mainGallery.insertAdjacentHTML("beforeend", makeGalleryItems);
+
+mainGallery.addEventListener("click", function (event) {
+  event.preventDefault();
+});
+
+const createModalWindow = (event) => {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  mainGallery.instance = basicLightbox.create(
+    `
+    <img src= "${event.target.dataset.source}" />
+`,
+    {
+      onShow: () =>
+        mainGallery.addEventListener("keydown", closeModalWindowByEsc),
+      onClose: () => {
+        mainGallery.removeEventListener("keydown", closeModalWindowByEsc);
+      },
+    }
+  );
+  mainGallery.instance.show();
+};
+
+const closeModalWindowByEsc = (event) => {
+  event.preventDefault();
+  const escape = "Escape";
+  if (event.code === escape && mainGallery.instance.visible()) {
+    mainGallery.instance.close();
+  }
+};
+
+mainGallery.addEventListener("click", createModalWindow);
